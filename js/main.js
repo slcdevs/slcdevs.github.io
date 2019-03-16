c02cd0e7b086d7358f38c00da7272908822f4fde
Chase Pierce <syntaqx@gmail.com>
2019-03-15 18:07:29 -0600

    normalization for jekyll
    - also added some JS integrity checks
    - ensure we can build locally and bundle install works

--- js/main.js
+++ js/main.js
@@ -1,19 +1,32 @@
 $(document).ready(function () {
-  var myInit = { method: 'GET',
-               cache: 'default' };
+  var myInit = {
+    method: 'GET',
+    cache: 'default'
+  };
 
   fetch('https://cors-anywhere.herokuapp.com/https://api.meetup.com/slcdevs/events?&amp;sign=true&amp;photo-host=public&amp;page=20', myInit)
     .then(function (res) {
-      return res.json()
+      return res.json();
     })
-    .then(function (myJson) {
-      let title = myJson[0].name;
-      let description = myJson[0].description;
-      let link = myJson[0].link;
-      let address = myJson[0].venue.address_1;
-      let venue = myJson[0].venue.name;
-      let date = myJson[0].local_date;
-      let time = myJson[0].local_time;
+    .then(function (data) {
+      if (data.length == 0) {
+        return;
+      }
+
+      let title = data[0].name;
+      let description = data[0].description;
+      let link = data[0].link;
+      let address = ''
+      let venue = '';
+
+      if (data[0].venue != null) {
+        address = data[0].venue.address_1;
+        venue = data[0].venue.name
+      }
+
+      let date = data[0].local_date;
+      let time = data[0].local_time;
+
       let html = `
       <a href='${link}'>
       <h2>${title}</h2>
@@ -22,6 +35,8 @@ $(document).ready(function () {
       <h3>${date} ${venue} ${address} ${time}</h3>
       <br>${description}
       `;
-      $("#jumbotron").html(html)
-    })
+
+      $("#jumbotron").html(html);
+
+    });
 });
