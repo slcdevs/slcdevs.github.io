7ad1c8f0ecab36a4c844aec56f266a117b5ae12e
Chase Pierce <syntaqx@gmail.com>
2019-04-24 16:42:50 -0600

    fix venue

--- js/main.js
+++ js/main.js
@@ -10,17 +10,17 @@ fetch('https://cors-anywhere.herokuapp.com/https://api.meetup.com/slcdevs/events
     return;
   }
 
-  let venue = '';
-  let address = '';
-
-  if (data[0].venue != null) {
-    venue = data[0].venue.name
-    address = data[0].venue.address_1;
-  }
-
   data.forEach(function(event, i) {
     if (i > 2) {
       return;
+    }
+
+    let venue = '';
+    let address = '';
+
+    if (event.venue != null) {
+      venue = event.venue.name
+      address = event.venue.address_1;
     }
 
     $("#events").append(`
